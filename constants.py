1c8b33835857414d507b52bc593681522ce0f4d8
Abdullah Wahid <abdullah.wahid@intellixcore.com>
2025-02-02 17:12:05 +0300

    Completed testing with locked packages

---
+++ constants.py
@@ -0,0 +1,25 @@
+class constants:
+    libraries = {
+    "data_analysis": [
+        "pandas", "numpy", "polars", "dask", "pyarrow"
+    ],
+    "chart_plotting": [
+        "matplotlib", "seaborn", "plotly", "bokeh", "altair", "folium", "networkx"
+    ],
+    "data_preprocessing": [
+        "scikit-learn", "category_encoders", "missingno", 
+        "sklearn.preprocessing", "imbalanced-learn"
+    ],
+    "statistical_analysis": [
+        "scipy.stats", "statsmodels", "pingouin"
+    ],
+    "time_series_analysis": [
+        "statsmodels.tsa", "prophet", "tsfresh", "sktime"
+    ],
+    "big_data_processing": [
+        "pyspark", "vaex", "modin"
+    ],
+    "geospatial_analysis": [
+        "geopandas", "shapely", "rasterio"
+    ]
+}
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import fire
 
 from metagpt.roles.di.data_interpreter import DataInterpreter
+from constants import constants
 
 DATA_DIR = "test_df"
 # sales_forecast data from https://www.kaggle.com/datasets/aslanahmedov/walmart-sales-forecast/data
-REQUIREMENT = f"""do some sentiment analysis over the Claim_Description column, Here is the file path: {DATA_DIR}.csv"""
+REQUIREMENT = f"""do some sentiment analysis over the Claim_Description column, Here is the file path: {DATA_DIR}.csv
+You are only allowed to use the following libraries for the code you generate in this task: {constants.libraries}"""
 
 
 async def main():
